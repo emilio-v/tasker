94eabc86433628233979e9a6ddd9ef2228f8569c
Emilio Velasco <emilio_785@hotmail.com>
2023-07-11 22:26:13 -0600

    create columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
 import { Layout } from "@/components/layouts";
-import { Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant="h1" color="primary">
-        Hello
-      </Typography>
+    <Layout title="Home | Tasker">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="TO DO" />
+
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="IN PROGRESS" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="COMPLETED" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
